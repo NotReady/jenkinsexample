4bd74f319d5714eda874d10e213a7fe9dcfd1578
msohashi <ohashi@musicsecurities.com>
2019-09-20 14:55:04 +0900

    コピーパス修正

--- cp.js
+++ cp.js
@@ -2,11 +2,11 @@ const fs = require("fs-extra");
 
 module.exports.scCopy = function() {
     // スクショdir
-    const parentDir = "./screen_shot/";
-// 保存先dir
+    const parentDir = "screen_shot/";
+    // 保存先dir
     const saveDir = parentDir + "latest";
 
-// 旧最新保存を削除
+    // 旧最新保存を削除
     try {
         fs.statSync(saveDir);
         fs.removeSync(saveDir);
@@ -17,11 +17,11 @@ module.exports.scCopy = function() {
         }
     }
 
-// 最新dir
+    // 最新dir
     const nameDirsHistory = fs.readdirSync(parentDir);
     nameLatestDir = parentDir + nameDirsHistory[nameDirsHistory.length - 1];
 
-// 最新dirを保存する
+    // 最新dirを保存する
     fs.copySync(nameLatestDir, saveDir);
 
     console.log("copy screentshot completed src:" + nameLatestDir + " dst:" + saveDir);
