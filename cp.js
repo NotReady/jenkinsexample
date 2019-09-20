abc2ff1abbd47db3f9cc3335d85a32db8a03b810
msohashi <ohashi@musicsecurities.com>
2019-09-20 13:18:53 +0900

    スクショの最新コピー処理

---
+++ cp.js
@@ -0,0 +1,24 @@
+const fs = require("fs-extra");
+
+// スクショdir
+const parentDir = "./screen_shot/";
+// 保存先dir
+const saveDir = parentDir + "latest";
+
+// 旧最新保存を削除
+try {
+    fs.statSync(saveDir);
+    fs.removeSync(saveDir);
+}catch(err){
+    if( err.code !== 'ENOENT'){
+        console.log(err.message);
+        throw err;
+    }
+}
+
+// 最新dir
+const nameDirsHistory = fs.readdirSync(parentDir);
+nameLatestDir = parentDir + nameDirsHistory[nameDirsHistory.length-1];
+
+// 最新dirを保存する
+fs.copySync(nameLatestDir, saveDir);
